b785c459f09ac0fe8f8f27177e5097172b1c3726
Subodh Pradhan <subodh@99planes.com>
2021-03-04 21:22:29 +0545

    Set root url test

--- lib/api/getRootUrl.ts
+++ lib/api/getRootUrl.ts
@@ -1,7 +1,7 @@
 export default function getRootUrl() {
   const port = process.env.PORT || 3001
   const dev = process.env.NODE_ENV !== 'production'
-  const ROOT_URL = dev ? `http://localhost:${port}` : 'http://localhost:3000'
+  const ROOT_URL = dev ? `http://localhost:${port}` : 'https://d3614fxonhv3cv.cloudfront.net'
 
   return ROOT_URL
 }
