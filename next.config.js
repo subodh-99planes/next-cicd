18fe7f2983f2ed3f5fc74322d6a4c201b8a9edc6
Subodh Pradhan <subodh@99planes.com>
2021-03-06 19:28:42 +0545

    test4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     includePaths: [path.join(__dirname, 'pages'), path.join(__dirname, 'features')],
   },
   env: {
-    PORT: 3002,
+    PORT: 8081,
     localStorageKey: 'is_survey',
     userdataCookie: 'is_session',
     url: 'http://localhost:3002'
